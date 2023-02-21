378f7b65c8b06209c5143de2f892f90d1c2cb417
Guilloux <cecile.guilloux@silecs.info>
2023-02-21 13:28:00 +0100

    JS : Ajout de la fonctionnalité dans activité feedback - theme adaptable

--- lib.php
+++ lib.php
@@ -40,6 +40,31 @@ EOJS
             
             }
         }
+    } else if ($PAGE->theme->name == 'adaptable') {
+        $pages = ['/mod/feedback/view.php'];
+        if ($PAGE->has_set_url() && in_array($PAGE->url->get_path(), $pages)) {
+             if ($DB->get_records('config_plugins', array('plugin' => 'local_up1_notification'))) {
+                $id = $PAGE->url->get_param('id');
+                $lien = new moodle_url('/local/up1_notification/notification.php', ['id' => $id]);
+                $classa = 'btn btn-secondary';
+                $linktab = '<li class="navitem"><a href="' . htmlspecialchars($lien)
+                    . '" class="' . $classa . '"' 
+                    . ' title="' . htmlspecialchars(get_string("notifications")) 
+                    . '" role="menuitem">'
+                    . htmlspecialchars(get_string("notifications")) . '</a></li>';
+            
+                $enc = json_encode($linktab);
+                $PAGE->requires->js_init_code(<<<EOJS
+            var boutons = document.querySelectorAll('#region-main div[role="main"] div.tertiary-navigation div.row');
+            if (boutons) {
+                var barreb = boutons[0];
+                barreb.insertAdjacentHTML('beforeend', $enc);
+            }
+        
+EOJS
+        , true);
+            }
+        }
     }
 }
 
