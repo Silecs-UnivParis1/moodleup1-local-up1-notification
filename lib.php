9873c7e35602d28bb2c9d9512dddb59243d3141d
Guilloux <cecile.guilloux@silecs.info>
2023-02-21 12:25:41 +0100

    JS : Ajout de la fonctionnalité dans activité feedback - theme boost

--- lib.php
+++ lib.php
@@ -8,37 +8,38 @@
 
 function local_up1_notification_extend_navigation() {
     global $OUTPUT, $PAGE, $DB;
-    
-    $pages = ['/mod/feedback/edit.php', '/mod/feedback/view.php', 
-		'/mod/feedback/analysis.php', '/mod/feedback/show_entries.php', 
-		'/local/up1_notification/notification.php'];
-	if ($PAGE->has_set_url() && in_array($PAGE->url->get_path(), $pages)) {
-		$classa = 'nav-link';
-		if ($PAGE->url->get_path() === '/local/up1_notification/notification.php') {
-			$classa .= ' active';
-		}
-		if ($DB->get_records('config_plugins', array('plugin' => 'local_up1_notification'))) {
-			$id = $PAGE->url->get_param('id');
-			$lien = new moodle_url('/local/up1_notification/notification.php', ['id' => $id]);
-			$linktab = '<li class="nav-item"><a href="' . htmlspecialchars($lien)
-				. '" class="' . $classa . '"' . ' title="' . htmlspecialchars(get_string("notifications")) . '">'
-				. htmlspecialchars(get_string("notifications")) . '</a></li>';
-		
-			//style Boost
-			$enc = json_encode($linktab);
-			$PAGE->requires->js_init_code(<<<EOJS
-   
-    var onglets = document.querySelectorAll('#region-main div[role="main"] ul.nav');
-    if (onglets) {
-		for (var i = 0; i < onglets.length; i++) {
-			var ul = onglets[i];
-			ul.insertAdjacentHTML('beforeend', $enc);
-		}
-    }   
+    if ($PAGE->theme->name == 'boost') {
+        $pages = ['/mod/feedback/edit.php', '/mod/feedback/view.php', 
+            '/mod/feedback/analysis.php', '/mod/feedback/show_entries.php',
+            '/mod/feedback/manage_templates.php', 
+            '/local/up1_notification/notification.php'];
+        if ($PAGE->has_set_url() && in_array($PAGE->url->get_path(), $pages)) {
+            $classa = 'nav-link';
+            if ($PAGE->url->get_path() === '/local/up1_notification/notification.php') {
+                $classa .= ' active';
+            }
+            if ($DB->get_records('config_plugins', array('plugin' => 'local_up1_notification'))) {
+                $id = $PAGE->url->get_param('id');
+                $lien = new moodle_url('/local/up1_notification/notification.php', ['id' => $id]);
+                $linktab = '<li class="nav-item"><a href="' . htmlspecialchars($lien)
+                    . '" class="' . $classa . '"' 
+                    . ' title="' . htmlspecialchars(get_string("notifications")) 
+                    . '" role="menuitem">'
+                    . htmlspecialchars(get_string("notifications")) . '</a></li>';
+            
+                $enc = json_encode($linktab);
+                $PAGE->requires->js_init_code(<<<EOJS
+               
+        var ongletrep = document.querySelectorAll('#topofscroll div.secondary-navigation ul[role="menubar"] li[data-key="responses"]');
+        if (ongletrep) {
+            var li = ongletrep[0];
+            li.insertAdjacentHTML('afterend', $enc);
+        }   
 EOJS
-    , true);
-		
-		}
-	}
+        , true);
+            
+            }
+        }
+    }
 }
 
